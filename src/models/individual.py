f7e606167995e75bcd739496e79ad14e02362e4a
Ricardo Araujo <ricardoasf@id.uff.br>
2026-03-05 16:26:05 -0300

    feat: add initial Individual class

---
+++ src/models/individual.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Individual():
+    prompt: str
+    fitness: float
--- src/selection/base.py
+++ src/selection/base.py
@@ -1,6 +1,8 @@
+from src.models.individual import Individual
+
 from abc import ABC, abstractmethod
 
 class Selection(ABC):
     @abstractmethod
-    def select(self, offsprings: list, pop_size: int):
+    def select(self, offsprings: list[Individual], pop_size: int):
         pass
