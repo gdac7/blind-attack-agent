7d091f5deb9d936361c51720b56a3e72c09cd58c
Ricardo Araujo <ricardoasf@id.uff.br>
2026-03-09 17:33:34 -0300

    feat: add Tournament selection strategy

---
+++ src/selection/tournament.py
@@ -0,0 +1,19 @@
+from src.selection.base import Selection
+from src.models.individual import Individual
+
+import random
+
+class TournamentSelection(Selection):
+    def __init__(self, tournament_size: int = 3):
+        self.tournament_size = tournament_size
+    
+    def select(self, offsprings: list[Individual], pop_size: int, num_selected: int) -> list[Individual]:
+        selected = []
+
+        for _ in range(num_selected):
+            tournament_pool = random.sample(offsprings, self.tournament_size)
+
+            winner = max(tournament_pool, key=lambda ind: ind.fitness)
+            selected.append(winner)
+
+        return selected
