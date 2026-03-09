02856231c2867fd6b1be1b5ebd982b46a6c374bd
Ricardo Araujo <ricardoasf@id.uff.br>
2026-03-09 16:17:49 -0300

    feat(Individual): add data seeding

--- src/models/individual.py
+++ src/models/individual.py
@@ -4,3 +4,13 @@ from dataclasses import dataclass
 class Individual():
     prompt: str
     fitness: float
+
+def mock_individuals() -> list[Individual]:
+    mocks = []
+    mocks.append(Individual("Galo", 9))
+    mocks.append(Individual("Raposa", 7))
+    mocks.append(Individual("Biriba", 5))
+    mocks.append(Individual("Cartola", 3))
+    mocks.append(Individual("Saci", 6))
+
+    return mocks
