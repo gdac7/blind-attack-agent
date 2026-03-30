b32a2e7339dfc849a52ed70edb7844f6235b6eec
Ricardo Araujo <ricardoasf@id.uff.br>
2026-03-30 16:20:10 -0300

    feat(SAOptimizer): add initial anneal individual function

--- src/optimizers/sa/optimizer.py
+++ src/optimizers/sa/optimizer.py
@@ -3,6 +3,7 @@ from src.optimizers.fitness.base import FitnessFunction
 from src.optimizers.models.individual import Individual
 
 import math
+import random
 
 class SAOptimizer(Optimizer):
     DEFAULT_INITIAL_TEMP = 100
@@ -30,3 +31,28 @@ class SAOptimizer(Optimizer):
         neighbor = Individual(neighbor_prompt)
 
         return neighbor
+    
+    def _anneal_individual(self, initial_solution: Individual) -> Individual:
+        temp = self._calibrate_temperature(initial_solution)
+
+        curr_solution = initial_solution
+        curr_fitness = self.evaluator.evaluate(initial_solution)
+
+        for i in range(self.max_iterations):
+            neighbor = self._generate_neighbor(curr_solution)
+            neighbor_fitness = neighbor.fitness
+
+            if neighbor_fitness < curr_fitness:
+                curr_solution = neighbor
+                curr_fitness = neighbor_fitness
+            else:
+                delta_e = neighbor_fitness - curr_fitness
+                metropolis_prob = self._metropolis(temp, delta_e)
+
+                if random.random() < metropolis_prob:
+                    curr_solution = neighbor
+                    curr_fitness = neighbor_fitness
+            
+            temp = temp * self.cooling_rate
+
+        return curr_solution
