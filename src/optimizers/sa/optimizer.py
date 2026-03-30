05a215d7a6cca6f2b3bcc2b3edef916aeb8649b3
Ricardo Araujo <ricardoasf@id.uff.br>
2026-03-30 15:41:51 -0300

    feat(SAOptimizer): add initial calibrate temperature function and refactor class magic numbers

--- src/optimizers/sa/optimizer.py
+++ src/optimizers/sa/optimizer.py
@@ -5,16 +5,25 @@ from src.optimizers.models.individual import Individual
 import math
 
 class SAOptimizer(Optimizer):
+    DEFAULT_INITIAL_TEMP = 100
+    DEFAULT_COOLING_RATE = 0.99
+    DEFAULT_MAX_ITER     = 20
+
     def __init__(
         self,
         evaluator: FitnessFunction,
-        cooling_rate: float = 0.99
+        cooling_rate: float = DEFAULT_COOLING_RATE,
+        max_iterations: int = DEFAULT_MAX_ITER
     ):
-        self.evaluator = evaluator,
+        self.evaluator = evaluator
         self.cooling_rate = cooling_rate
+        self.max_iterations = max_iterations
     
     def _metropolis(self, temperature: float, delta_e: float) -> float:
         return math.exp(-(delta_e) / temperature)
+    
+    def _calibrate_temperature(self, initial_solution: Individual) -> float:
+        return self.DEFAULT_INITIAL_TEMP
     
     def _generate_neighbor(self, current_solution: Individual) -> Individual:
         neighbor_prompt = current_solution.prompt + '!'
