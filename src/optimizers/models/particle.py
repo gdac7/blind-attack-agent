4b84169c161ea39e6e00b20f6b5df738e22fa693
Ricardo Araujo <ricardoasf@id.uff.br>
2026-04-05 16:53:38 -0300

    feat(Particle): add evaluate_curr method

--- src/optimizers/models/particle.py
+++ src/optimizers/models/particle.py
@@ -1,5 +1,7 @@
 from src.optimizers.models.individual import Individual
+from src.optimizers.fitness.base import FitnessFunction
 
+import copy
 from dataclasses import dataclass
 
 @dataclass
@@ -7,4 +9,12 @@ class Particle:
     curr_state: Individual
     particle_best: Individual | None = None
     velocity: int = 1
+    
+    def evaluate_curr(self, evaluator: FitnessFunction) -> float:
+        evaluator.evaluate(self.curr_state)
+
+        if (not self.particle_best) or (self.curr_state.fitness > self.particle_best.fitness):
+            self.particle_best = copy.deepcopy(self.curr_state)
+
+        return self.curr_state.fitness
     
