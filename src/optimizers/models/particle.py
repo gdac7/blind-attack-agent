04d46f15df924ca68c174be6fc6c946e1d667978
Ricardo Araujo <ricardoasf@id.uff.br>
2026-04-05 17:44:52 -0300

    feat(Particle): add from_individual class method

--- src/optimizers/models/particle.py
+++ src/optimizers/models/particle.py
@@ -7,4 +7,8 @@ class Particle:
     curr_state: Individual
     particle_best: Individual | None = None
     velocity: int = 1
+
+    @classmethod
+    def from_individual(cls, individual: Individual) -> 'Particle':
+        return cls(curr_state=individual)
     
